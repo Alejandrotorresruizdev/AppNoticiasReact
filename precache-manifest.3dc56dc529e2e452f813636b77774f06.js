622bb0894877e6b01f9fb948a8bf50084ac42f27
OpenCLoud <alejandro.torres@opencloud.es>
2020-04-03 13:26:53 +0200

    Updates

---
+++ precache-manifest.3dc56dc529e2e452f813636b77774f06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d748f7447969a369e50ffb5ac33127d",
+    "url": "/AppNoticiasReact/index.html"
+  },
+  {
+    "revision": "95c55fb9700e0da70fed",
+    "url": "/AppNoticiasReact/static/css/main.ef46154e.chunk.css"
+  },
+  {
+    "revision": "53b68bb7d2d1d8307c28",
+    "url": "/AppNoticiasReact/static/js/2.f36752ae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/AppNoticiasReact/static/js/2.f36752ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95c55fb9700e0da70fed",
+    "url": "/AppNoticiasReact/static/js/main.0d251eef.chunk.js"
+  },
+  {
+    "revision": "dbb90266d151956dcbb3",
+    "url": "/AppNoticiasReact/static/js/runtime-main.df986d38.js"
+  }
+]);
